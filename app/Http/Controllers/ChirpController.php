9ba239248640a496db33e89b96ef3591131a35d3
Masahiro111 <33553834+Masahiro111@users.noreply.github.com>
2023-01-11 20:00:00 +0900

    Showing Chirps

--- app/Http/Controllers/ChirpController.php
+++ app/Http/Controllers/ChirpController.php
@@ -14,7 +14,12 @@ class ChirpController extends Controller
      */
     public function index()
     {
-        return view('chirps.index');
+        $chirps = Chirp::query()
+            ->with('user')
+            ->latest()
+            ->get();
+
+        return view('chirps.index', compact('chirps'));
     }
 
     /**
